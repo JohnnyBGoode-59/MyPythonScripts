6d79bd21996d1e5cd3d0ba1c05cd06649040641b
John Eichenberger <johnandjanita@gmail.com>
2020-08-20 13:07:23 -0700

    Add Camera-Backup.py and Photo-Renamer.py
    I wrote these two programs to help me manage the thousands of pictures
    I have taken over the last twenty plus years.
    
    -Ike-

---
+++ EXIF_Dating.py
@@ -0,0 +1,108 @@
+#-------------------------------------------------------------------------------
+# Name: EXIF-Dating
+# Purpose: Use pattern matching to determine the date of a file from the filename
+#
+# Author: John Eichenberger
+#
+# Created: 19/08/2020
+# Copyright:   (c) John Eichenberger 2020
+#-------------------------------------------------------------------------------
+
+import re, os
+from PIL import Image
+from PIL.ExifTags import TAGS
+import piexif
+
+today = ["2020", "08", "19", "12", "00", "00"]
+
+def get_exif(fn):
+    """ Get the EXIF data from a file, if it is available """
+    ret = {}
+    try:
+        i = Image.open(fn)
+    except:
+        print("failed: Image.open({})".format(fn))
+        return ret
+    try:
+        info = i._getexif()
+    except:
+        print("failed: _getexif: {}".format(fn))
+        return ret
+
+    if info is not None:
+        for tag, value in info.items():
+            decoded = TAGS.get(tag, tag)
+            # if decoded != tag:
+            #    print("{} {}({})={}".format(fn, decoded, tag, value))
+            ret[decoded] = value
+        # print("{} has {} EXIF data elements".format(fn, len(ret)))
+    else:
+        # print("{} has no EXIF data".format(fn))
+        pass
+    return ret
+
+def GetExifDate(pn):
+    """ Get a date from a picture using EXIF data """
+    exif = get_exif(pn)
+    # print(exif)
+    ret = exif.get('DateTime')
+    if ret is None:
+        ret = exif.get('DateTimeOriginal')
+    if ret is None:
+        ret = exif.get('DateTimeDigitized')
+    if ret is not None:
+        ret = [ ret[0:4], ret[5:7], ret[8:10], ret[11:13], ret[8:10], ret[11:13] ]
+    return ret
+
+def SetExifDate(pn, date):
+    """ Add EXIF data to set a date """
+    if len(date) == 6:
+        dstr = date[0]+':'+date[1]+':'+date[2]+' '+date[3]+':'+date[4]+':'+date[5]
+    else:
+        dstr = date[0]+':'+date[1]+':'+date[2]+' 12:00:00'
+
+    # Add DateTimeOriginal to the Exif data
+    exif = piexif.load(pn)
+    exif['Exif'][36867] = dstr  # "DateTimeOriginal"
+    exif_encoded = piexif.dump(exif)
+
+    # Replace the original file with the new file and exif
+    im = Image.open(pn)
+    # im.close()
+    # os.remove(pn)
+    im.save(pn, "jpeg", exif=exif_encoded)
+    return
+
+def GetFileDate(fn):
+    """ Use a series of regular expressions to extract the date from a filename """
+    global today
+
+    # Allow any separator in this list
+    sep = "[\-\._~ ]*?" # optional separators: the dash has to be escaped
+
+    # yyyy-mm-dd_hh-mm-ss (yyyy must start with 19 or 20)
+    yyyy = "(19[0-9][0-9]|20[0-9][0-9])"+sep
+    mm = dd = "([0-9][0-9])"+sep
+    hr = min = sec = "([0-9][0-9])"+sep
+    m = re.search("^"+yyyy+mm+dd+hr+min+sec, fn)
+    if m is not None:
+        for i in range(6):
+            if m.group(i+1) is not None:
+                today[i] = m.group(i+1)
+        return today
+
+    # yyyy-mm-dd_ (yyyy must start with 19 or 20)
+    m = re.search("^"+yyyy+mm+dd, fn)
+    if m is not None:
+        for i in range(3):
+            if m.group(i+1) is not None:
+                today[i] = m.group(i+1)
+        return today[:3]
+
+    return None
+
+def main():
+    GetFileDate('195907252004')
+
+if __name__ == '__main__':
+    main()
---
+++ Photo-Renamer.py
@@ -0,0 +1,90 @@
+#-------------------------------------------------------------------------------
+# Name: Photo-Renamer
+# Purpose:  Rename photos based upon the EXIF date for those photos.
+#   Add a EXIF date when the filename contains a date but the EXIF data is missing.
+#
+# Author: John Eichenberger
+#
+# Created:     12/08/2020
+# Copyright:   (c) John Eichenberger 2020
+#-------------------------------------------------------------------------------
+
+found = 0
+renamed = 0
+dated = 0
+
+import glob, re, os, sys
+
+from EXIF_Dating import GetExifDate, SetExifDate, GetFileDate
+
+def rename(pn):
+    """ Archive one picture or movie in it's proper place """
+    global found, renamed, dated
+    found = found + 1
+
+    # Try to get both EXIF date and File date
+    exif_date = GetExifDate(pn)
+    rootp, fn = os.path.split(pn)
+    file_date = GetFileDate(fn)
+
+    # If the file has no EXIF date
+    if exif_date is None:
+        # but a file_date is available
+        if file_date is None:
+            print("{} has no date".format(pn))
+            return
+
+        # set the EXIF date
+        print("Add a date to {}".format(pn))
+        SetExifDate(pn, file_date)
+        dated = dated + 1
+        return
+
+    else:
+        # If the two dates match, do nothing
+        if file_date is not None:
+            if (exif_date[:len(file_date)] == file_date):
+                print("{} is well named".format(pn))
+                return
+
+    # Define the new prefix for the filename
+    # prefix = d[0:4]+'-'+d[5:7]+'-'+d[8:10]+'_'+d[11:13]+'-'+d[14:16]+'-'+d[17:19]+'_' # yyyy-dd-mm_hhmmss_
+    prefix = exif_date[0]+'-'+exif_date[1]+exif_date[2]+'_'+exif_date[3]+exif_date[4]+exif_date[5]+'_'
+
+    # Don't rename a file that is already renamed (this should be redundant now)
+    if fn[0:len(prefix)] == prefix:
+        print("{} is already renamed".format(fn))
+        return
+
+    # Remove any previous date prefix
+    m = re.search("^([0-9~_\-\.]*)(.*)", fn)
+    if m is not None:
+        fn = m.group(2)
+
+    # Rename
+    newname = prefix + fn
+    os.rename(pn, rootp + '\\' + newname)
+    renamed = renamed + 1
+    print("{} renamed {}".format(pn, newname))
+
+def rename_everything(pn):
+    """ find everything and archive all pictures and movies """
+    if(os.path.isdir(pn)):
+        for fn in glob.glob(pn + "\\*"):
+            if(not os.path.isdir(fn)):
+                rename(fn)
+    else:
+        print("{} is not a folder".format(pn))
+
+def main(pn):
+    global found, renamed
+    rename_everything(pn)
+    print("Found {}, renamed {}, and added dates to {}".format(found, renamed, dated))
+
+if __name__ == '__main__':
+    # if a command line parameter is supplied it is used
+    # as the name of the folder to process
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        main(os.getcwd())
