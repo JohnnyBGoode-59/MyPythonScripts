56ec51b9dabc018ee9d4a8fd82279bbcbb71fef9
John Eichenberger <johnandjanita@gmail.com>
2020-08-23 13:46:36 -0700

    Process-Folders.cmd renames subfolders
    This command can get pretty intense as it can process a lot of files
    all at once.

---
+++ Process-Folders.cmd
@@ -0,0 +1,14 @@
+@echo off
+if exist 01Jan\*.* goto Start
+Echo This batch file loops through a set of folders named by dates
+Echo and renames and backs up those folders.
+exit /b
+
+:Start
+for /d %%d in (*) do (
+	cd %%d
+	photo-renamer.py
+	photo-backup.py -f
+	cd ..
+	rd %%d
+	)
