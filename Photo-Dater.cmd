acd72f089cfad177e5661d035e7ddb1aeebaa7b0
John Eichenberger <johnandjanita@gmail.com>
2020-08-22 19:17:42 -0700

    Add support for Photo-Dater -r switch
    When -r is supplied existing dates are reset using file dates.

--- Photo-Dater.cmd
+++ Photo-Dater.cmd
@@ -2,9 +2,10 @@
 if not "%2"=="" goto Run
 Echo This script will add a date prefix to a set of files.
 Echo It will then run Photo-Renamer.py to add that date to the EXIF data for those files.
-Echo Syntax: Photo-Dater {date} {files}
+Echo Syntax: Photo-Dater {date} {files} [-r]
 Echo To be safe the files that are specified should be in the current folder.
 Echo A subfolder will be temporarily created during processing and removed upon completion.
+Echo If the -r switch is supplied the EXIF dates will be reset
 exit /b
 
 :Run
@@ -22,7 +23,7 @@ for %%f in (%2) do (
 	cd ..
 	)
 cd %Folder%
-call photo-renamer.py
+call photo-renamer.py %3
 move *.* .. >nul
 cd ..
 rd %Folder%
---
+++ playground.cmd
@@ -0,0 +1,3 @@
+@echo off
+%HOMEDRIVE%
+cd %HOMEPATH%\Documents\Playground
