d91753faa38695cd99617dcd839712b61a2962ab
John Eichenberger <johnandjanita@gmail.com>
2021-06-12 05:58:36 -0700

    custom-renamer.cmd: sort the file
    That makes it easier to edit.

--- custom-renamer.cmd
+++ custom-renamer.cmd
@@ -2,7 +2,9 @@
 SetLocal
 set tfile=%temp%\rn.cmd
 if exist %tfile% del %tfile%
-for %%f in (%*) do echo ren "%%f" "%%f" >>%tfile%
+for %%f in (%*) do echo ren "%%f" "%%f" >>%tfile%.unsorted
+sort <%tfile%.unsorted >%tfile%
+del %tfile%.unsorted
 "C:\Program Files (x86)\Notepad++\notepad++.exe" %tfile%
 call %tfile%
 del %tfile%
