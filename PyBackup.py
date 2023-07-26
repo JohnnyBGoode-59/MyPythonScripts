eb092e860f446f4c6818c879abd986f522c8c09a
John Eichenberger <johnandjanita@gmail.com>
2023-07-26 08:14:27 -0700

    PyBackup: add verification, improve error handling
    The new -v switch can be used to verify that all data files match the
    current CRC control files.
    
    When a CRC control file is corrupt, it will be deleted and ignored.
    
    A log file is now created each time the program runs. It can be used to
    review the details of the exceptions that occurred during either a
    backup or verify operation.
    
    Make sure that a CRC is not added to the destination control file
    unless that file exists. Ensure that the destination CRC matches the
    file found at that location whenever a new CRC is actually computed.
    
    Backups can potentially be faster now because the destination CRC will
    never be computed twice.

--- PyBackup.py
+++ PyBackup.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Name: PyBackup
-# Purpose: A custom file backup program
+# Purpose: Backup files and use crc hashes to keep track of changes.
 #
 # Author: John Eichenberger
 #
@@ -18,18 +18,34 @@ copied = 0
 found = 0
 folders = 0
 hashes = 0
+corrupted = 0
+logfile = "backup.log.txt"  # opened in the %TEMP% folder
+
+def logerror(msg):
+    """ Log severe errors """
+    global logfile
+
+    if logfile != None:
+        log = open(logfile, 'a')
+        log.write(msg+'\r')
+        log.close()
+    print(msg + ' (logged)')
 
 def ReadCrcs(pn):
     """ Return a dictionary of CRC values for every file in a folder """
-    global crc_filename
+    global crc_filename, corrupted
     crcs = {}
+    filename = pn+'\\'+crc_filename
     try:
-        filename = pn+'\\'+crc_filename
         f = open(filename)
+    except: # open may not find the file
+        return crcs, None
+
+    try:
         for line in f:
             if line[-1] == '\n':
                 line = line[:-1]
-            list = line.split(',') # carefull, some names have commas
+            list = line.split(',') # careful, some names have commas
             crc = list[0]
             pn = list[1]
             for more in list[2:]:
@@ -38,8 +54,16 @@ def ReadCrcs(pn):
             crcs[fn] = crc
         f.close()
         last_modified = os.path.getmtime(filename)
-    except: # open may not find the file
+
+    except:
+        # The file must be corrupted. Delete it and discard all crcs.
+        logerror("Corrupt file removed: " + filename)
+        f.close()
+        os.remove(filename)
+        crcs = {}
         last_modified = None
+        corrupted = corrupted + 1
+
     return crcs, last_modified
 
 def AddCrc(pn, crcs):
@@ -50,7 +74,9 @@ def AddCrc(pn, crcs):
         crcs[fn] = crc32(pn)
         hashes = hashes + 1
     except: # crc32 may not find the file
-        crcs[fn] = None
+        if fn in crcs:
+            crcs.remove(fn)
+        logerror(pn + ": failure to compute CRC")
 
 def WriteCrcs(pn, crcs):
     """ Save a dictionary of CRC values for every file in a folder """
@@ -62,15 +88,56 @@ def WriteCrcs(pn, crcs):
             f.write(crcs[pn]+','+pn+'\n')
         f.close()
     except: # the above really should work, or else we have no CRCs
-        pass
+        logfile("Could not create: " + filename)
 
-##############################################################################
 def backup(src, dst):
     """ backup one source file """
     global copied
     print('copy "{}" "{}"'.format(src, dst))
-    copyfile(src, dst)
+    try:
+        copyfile(src, dst)
+    except:
+        logerror("copyfile(" + src + ',' + dst + "): failed")
     copied = copied + 1
+
+def verify(source):
+    """ check crc values for one folder """
+    global crc_filename, found, folders, corrupted
+
+    # Start by reading CRC files, if they exist
+    source_crcs, source_modified = ReadCrcs(source)
+    if source_modified == None:
+        logerror(source + ": Not validated, missing CRC file.")
+        return
+
+    print("Processing {}: {} folders, {} files, {} possibly corrupted".format(source, folders, found, corrupted))
+    folders = folders + 1
+
+    # Recursively search the source folder
+    for pn in glob.glob(source + '\\*'):
+        # print("*Debug* Found {}".format(pn))
+        rootp, fn = os.path.split(pn)
+        if os.path.isfile(pn):
+            if fn == crc_filename:
+                pass
+            else:
+                # For every file in the source folder
+                found = found + 1
+                try:
+                    rootp, fn = os.path.split(pn)
+                    if fn not in source_crcs:
+                        logerror(pn + ": missing CRC")
+                        corrupted = corrupted + 1
+                    elif source_crcs[fn] != crc32(pn):
+                        logerror(pn + ": mismatched CRC")
+                        corrupted = corrupted + 1
+                except: # crc32 may not find the file
+                    logerror(pn + ": failure to compute CRC")
+                    corrupted = corrupted + 1
+
+        elif os.path.isdir(pn):
+            # For every subfolder
+            verify(pn)
 
 ##############################################################################
 def main(source, dest):
@@ -78,7 +145,7 @@ def main(source, dest):
     global crc_filename, copied, found, folders
 
     # Start by reading CRC files, if they exist
-    print("Processing {}: {} folders, {} files, {} hashed {} copied".format(source, folders, found, hashes, copied))
+    print("Processing {}: {} folders, {} files, {} hashed, {} copied".format(source, folders, found, hashes, copied))
     folders = folders + 1
     source_crcs, source_modified = ReadCrcs(source)
     if not os.path.isdir(dest):
@@ -106,16 +173,13 @@ def main(source, dest):
                 # When source_modified is None, source_crcs is empty
                 if not fn in source_crcs or last_modified > source_modified:
                     AddCrc(pn, source_crcs)
-                if not fn in dest_crcs or last_modified > dest_modified:
+                if not fn in dest_crcs or last_modified > dest_modified or not exists(dest_pn):
+                    # Note that the destination CRC will be REMOVED when the file does not exist
                     AddCrc(dest_pn, dest_crcs)
 
-                # Skip files that have matching CRCs
-                if source_crcs[fn] == dest_crcs[fn]:
-                    continue    # matching files need not be backed up
-
-                # Backup the rest
-                backup(pn, dest_pn)
-                AddCrc(dest_pn, dest_crcs)
+                # Only backup files that do not have matching CRCs
+                if source_crcs[fn] != dest_crcs[fn]:
+                    backup(pn, dest_pn, dest_crcs)
 
         elif os.path.isdir(pn):
             # For every subfolder
@@ -127,30 +191,72 @@ def main(source, dest):
 
 ##############################################################################
 if __name__ == '__main__':
-    """ Use an ini file that identifies source and destination folders.
-        Copy all files in the source that do not exist in the destination.
-        Skip all files with matching CRCs in both locations.
-        Create a batch file that will copy newer files over older files,
-        but do not run that batch file.
-        Advanced ideas:
-            1. In the same folder as a file, save the CRC values for each file.
-            2. Use those values any time the CRC file is newer than the file.
+    """ There are three ways to use this program.
+        You can backup a single folder (and children) to a specified destination.
+        You can backup a set of folders to respective destinations using an ini file.
+        Or you can check the CRC integrety for a list of folders.
+
+        CRC values are used to make it possible to skip copying files that
+        have not changed since a previous backup.
     """
 
     # Time the backup
     start = time.time()
 
-    # Switch to the %USERPROFILE% folder where Backup.ini should reside.
-    userprofile = os.environ.get('USERPROFILE')
-    if userprofile is None:
-        print("USERPROFILE is not defined as an environment variable")
+    # (Re)Create a logfile used for severe errors
+    temp = os.environ.get('TEMP')
+    if temp is None:
+        print("TEMP is not defined as an environment variable")
+        logfile = None
+    else:
+        logfile = temp + '\\' + logfile
+        try:
+            os.remove(logfile)
+        except:
+            pass
+
+    # First look for the new -? switch
+    # PyBackup -?
+    if len(sys.argv) >= 2 and sys.argv[1] == "-?":
+        print(  "\nPyBackup can be used three ways"
+                "\n1) no parameters\t-- backup folders using ~\\backup.ini"
+                "\n2) souce dest\t\t-- backup one folder"
+                "\n3) -v [folders]\t\t-- check crcs in a list of folders")
         exit()
 
-    # If a source and destination is specified on the command line, use those
-    if len(sys.argv) == 3:  # command source destination
+    # Next look for the new -v switch, used to check CRCs for each folder listed
+    # PyBackup -v [folders]
+    if len(sys.argv) >= 2 and sys.argv[1] == "-v":
+        for pn in sys.argv[2:]:
+            verify(pn)
+        print("\nVerify complete.\n{} files, {} possibly corrupted".format(found, corrupted))
+        elapsed = time.gmtime(time.time()-start)
+        print("Completed in %02d:%02d:%02d" %(elapsed.tm_hour, elapsed.tm_min, elapsed.tm_sec))
+        exit()
+
+    # If a source and destination is specified on the command line, backup one folder
+    # PyBackup source destination
+    if len(sys.argv) == 3:
         main(sys.argv[1], sys.argv[2])
 
-    else:
+    if len(sys.argv) == 1:
+        """ No command line parameters were provided.
+            Use an ini file that identifies source and destination folders.
+            Copy all files in the source that do not exist in the destination.
+            Skip all files with matching CRCs in both locations.
+            Create a batch file that will copy newer files over older files,
+            but do not run that batch file.
+            Advanced ideas:
+                1. In the same folder as a file, save the CRC values for each file.
+                2. Use those values any time the CRC file is newer than the file.
+        """
+
+        # Switch to the %USERPROFILE% folder where Backup.ini should reside.
+        userprofile = os.environ.get('USERPROFILE')
+        if userprofile is None:
+            print("USERPROFILE is not defined as an environment variable")
+            exit()
+
         try:
             os.chdir(userprofile)
             f = open("Backup.ini")
@@ -166,6 +272,8 @@ if __name__ == '__main__':
                 main(source, dest)
         f.close()
 
-    print("\nBackup complete. {} files found. {} files copied.".format(found, copied))
+    print("\nBackup complete.\n{} files found. {} files copied.".format(found, copied))
+    if corrupted != 0:
+        print("{} corrupted crc files replaced.".format(corrupted))
     elapsed = time.gmtime(time.time()-start)
     print("Completed in %02d:%02d:%02d" %(elapsed.tm_hour, elapsed.tm_min, elapsed.tm_sec))
